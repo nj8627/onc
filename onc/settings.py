6f45a0e6379e1d8a3f1d10ff7bd424696a143f58
nj8627 <nitin.jindal@zsassociates.com>
2016-06-20 23:58:43 +0530

    fourthasss

--- onc/settings.py
+++ onc/settings.py
@@ -104,7 +104,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
